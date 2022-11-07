f9aafe0897141cacbf972a8b14ec481b78531dec
Roqi SB <roqisb9@gmail.com>
2022-11-07 16:57:59 +0700

    add public void to add a note in the list

--- Singly_linked_list/Program.cs
+++ Singly_linked_list/Program.cs
@@ -1,8 +1,39 @@
 ﻿using System;
 namespace single_linked_list
 {
+    // each node consist of the infomation part and link to the next node 
     class Node
     {
+        public int rollNumber;
+        public string name;
+        public Node next;
 
+    }
+    class List
+    {
+        Node START;
+        public List()
+        {
+            START = null;
+        }
+        public void addNote() //add a node in the list 
+        {
+            int rollNo;
+            string nm;
+            Console.Write("\nEnter the roll number of the student: ");
+            rollNo = Convert.ToInt32(Console.ReadLine());
+            Console.Write("\nEnter the roll name of the student: ");
+            nm = Console.ReadLine();
+            Node newnode = new Node();
+            newnode.rollNumber = rollNo;
+            newnode.name = nm;
+        }
+    }
+    class Program
+    {
+        static void Main(string[]args)
+        {
+
+        }
     }
 }
